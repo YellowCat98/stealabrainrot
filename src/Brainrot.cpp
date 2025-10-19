52159b93d6a529d1d70753509dd7ec53cb28cf47
YellowCat98 <elcucuyisgay@gmail.com>
2025-10-19 13:23:13 +0100

    make brainrots randomly wander around the screen, and also only run if the brainrot can wander

--- src/Brainrot.cpp
+++ src/Brainrot.cpp
@@ -23,13 +23,22 @@ bool Brainrot::init(const std::string& brainrotID) {
     float angle = utilities::random::random(0.0f, 1.0f) * 2 * std::numbers::pi;
     velocity = ccp(std::cosf(angle) * speed, std::sinf(angle) * speed);
 
-    this->scheduleUpdate();
+    if (!canWander) this->scheduleUpdate();
     return true;
 }
 
 void Brainrot::update(float dt) {
     auto winSize = CCDirector::get()->getWinSize();
     
+
+    float maxAngleChange = 15.0f * (std::numbers::pi / 180.0f);
+    float angleChange = utilities::random::random(-maxAngleChange, maxAngleChange);
+
+    float currentAngle = std::atan2f(velocity.y, velocity.x);
+    currentAngle += angleChange;
+
+    velocity = ccp(std::cosf(currentAngle) * speed, std::sinf(currentAngle) * speed);
+
     auto newPos = this->getPosition() + ccpMult(velocity, dt);
 
     if (newPos.x < 0 || newPos.x > winSize.width)
