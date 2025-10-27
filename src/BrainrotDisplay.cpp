4a40a0724071483d4abc0e30a118b97f4ac2eaa3
YellowCat98 <elcucuyisgay@gmail.com>
2025-10-27 20:18:42 +0100

    we love randomness!

--- src/BrainrotDisplay.cpp
+++ src/BrainrotDisplay.cpp
@@ -92,7 +92,7 @@ bool BrainrotDisplay::init() {
             bool runningOnLinux = false;
         #endif
         FLAlertLayer::create(
-            "Info",
+            utilities::random::randint(0, 1) ? "Info" : "Information",
             "Here lies all your collected brainrots.\n"
             "Duplicate brainrots are distinguished via the roman numeral after their name.\n"
             "The brainrot whose name is green is the current equipped brainrot.\n"
